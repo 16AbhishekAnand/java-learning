16448ca5195d4c4f4363948cf7dd4ae7d672bb01
Abhishek Anand <16abhishekanand@gmail.com>
2022-09-08 23:06:56 +0530

    pascalTriangle

---
+++ RightPascalTriangle.java
@@ -0,0 +1,21 @@
+
+public class RightPascalTriangle
+{
+    public static void RightPascalTriangle(int n)
+    {
+    int i,j,k,x,y,z;
+    for(i=0;i<n/2;i++){
+        for(j=0;j<=i;j++){
+            System.out.print("*");
+        }
+        System.out.print("\n");
+        
+    }
+    for(x=0;x<n-n/2;x++){
+        for(y=0;y<n-n/2-x-1;y++){
+            System.out.print("*");
+        }    
+        System.out.print("\n");
+    }
+    }
+}
